2cbc1c6748eb8ccb125f77e5741c5274a93c812e
Doring <d0211468@kowon.dongseo.ac.kr>
2021-09-06 09:07:49 +0900

    Golang Hello world 예제 각 코드 설명

---
+++ Golang/1_HelloWorld.go
@@ -0,0 +1,9 @@
+// https://www.youtube.com/watch?v=Of-mGH8OLT8&list=PLy-g2fnSzUTAaDcLW7hpq0e8Jlt7Zfgd6&index=7
+
+package main // 패키지 선언
+
+import "fmt" // main의 fmt를 포함. fmt는 표준 패키지이다.
+
+func main() {
+	fmt.Println("Hello Golang World!") //fmt에 있는 Println 함수를 call한다.
+}
