284ee3ff1c71f93f1a46b22300016c7d583069cf
Doring <d0211468@kowon.dongseo.ac.kr>
2021-09-11 21:23:57 +0900

    정수 계산 및 출력

---
+++ Golang/3_calculate_int.go
@@ -0,0 +1,18 @@
+// https://www.youtube.com/watch?v=hrXudfFOEPY&list=PLy-g2fnSzUTAaDcLW7hpq0e8Jlt7Zfgd6&index=9
+
+package main
+
+import "fmt"
+
+func main() {
+	// 정수 계산
+	var a int
+	var b int
+	a = 10
+	b = 2
+
+	fmt.Println(a + b)
+	fmt.Println(a - b)
+	fmt.Println(a * b)
+	fmt.Println(a / b)
+}
